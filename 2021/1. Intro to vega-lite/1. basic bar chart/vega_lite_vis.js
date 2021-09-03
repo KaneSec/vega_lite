dbfc50e464c3a30c09894cccfe42a566fb7e56f1
Kane <kanesecond@gmail.com>
2021-09-04 00:58:43 +1000

    changes

--- 2021/1. Intro to vega-lite/1. basic bar chart/vega_lite_vis.js
+++ 2021/1. Intro to vega-lite/1. basic bar chart/vega_lite_vis.js
@@ -1,4 +1,4 @@
-var spec2 = "js/daily_cost_bar_chart.json";
-vegaEmbed('#bar_chart', spec2).then(function(result) {
+var vg_1 = "daily_cost_bar_chart.vg.json";
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
