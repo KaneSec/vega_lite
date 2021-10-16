aaef8dc4237115aeb6d3566964858b4df6592e38
Kane <kanesecond@gmail.com>
2021-10-16 23:34:45 +1100

    c

--- 0_students_questions/3179week9-main/js/script.js
+++
@@ -1,5 +0,0 @@
-  var spec2 = "js/employment.vl.json";
-  //vegaEmbed('#employment_map', spec2);
-  vegaEmbed('#employment_map', spec2).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-  }).catch(console.error);
